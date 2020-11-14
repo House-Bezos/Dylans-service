edd4d1948ef9536039b5a13535a251e714665cff
Dylan Ring <dylanRing.BS@gmail.com>
2020-11-14 16:07:30 -0700

    Seed and Database working with some react completed on the side

---
+++ src/ProductEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductEntry = ({data}) => (
+  <div>
+    <div>name</div>
+    <div>rating, numRatings</div>
+    <div>price, prime</div>
+  </div>
+);
+
+
+export default ProductEntry;
--- src/Products-list.jsx
+++ src/Products-list.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import ProductEntry from './ProductEntry.jsx';
 
-const ProductList = (props) => {
+const ProductList = ({data, page}) => { // display 7 products at a time
+  //console.log(page);
+  // page 1 products 0-6, page 2 products 7-13, allow partial page population, i.e. < 7 products on a page
+  let startIndex = (page - 1) * 7;
+  let pageProducts = [];
+  for (var i = 0; i < 7; i++) {
+    // if (data[startIndex + i] !== undefined) { // breaks
+    //   pageProducts.push(data[startIndex + i]);
+    // }
+  }
+  return (
+    <div>
+      <ProductEntry/>
+    </div>
 
+  );
 }
+
+// {pageProducts.map((product) => (
+//   <ProductEntry props={product}/>
+// ))}
 
 export default ProductList;
